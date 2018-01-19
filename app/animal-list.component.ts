0069ddb7488a2e3d02f94cf70d41ccfd868c620c
cj <thatcat13@gmail.com>
2018-01-19 09:43:00 -0800

    add main animal list in root component and discover the zoo will be fantastic mr fox themed

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 
 
 @Component({
   selector: 'animal-list',
   template: `
-<h3>Animal list is here</h3>
+    <ul>
+      <div *ngFor='let currentAnimal of childAnimalList'>
+        <h4>Species: {{currentAnimal.species}}<button (click)="editButtonHasBeenClicked(currentAnimal)">Update Animal</button></h4>
+        <h3>Name: {{currentAnimal.name}}</h3>
+        <h3>Age: {{currentAnimal.age}}</h3>
+        <h3>Diet: {{currentAnimal.diet}}</h3>
+        <h3>Location: {{currentAnimal.location}}</h3>
+        <h3>Caretakers: {{currentAnimal.caretakers}}</h3>
+        <h3>Sex: {{currentAnimal.sex}}</h3>
+        <h3>Likes: {{currentAnimal.likes}}</h3>
+        <h3>Dislikes: {{currentAnimal.dislikes}}</h3>
+      </div>
+    </ul>
   `
 })
 
 export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(animalToEdit: Animal) {
+  this.clickSender.emit(animalToEdit);
+}
 
 }
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,11 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Local Zoo</h1>
+  <div class="container">
+    <h1>Fantastic Zoo</h1>
+    <h3>{{currentAnimalList}}</h3>
+    <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
+  </div>
   `
 })
 
@@ -13,6 +17,21 @@ import { Animal } from './animal.model';
 export class AppComponent {
   currentAnimalList: string = 'Animals currently residing here: ';
   //component class is what this is declares how component BEHAVES
+
+  masterAnimalList: Animal[] = [
+  new Animal('Lutra Lutra', 'European Otter', 'Linda Otter', 3, 'pescetarian', 'River Zone', 2, 'F', 'fish, typing', 'loud noises'),
+
+  new Animal('Talpa Europa', 'European Mole', 'Mole', 2, 'carnivore', 'Underground Zone', 2, 'M', 'earthworms', 'bright light'),
+
+  new Animal('Oryctolagus Cuniculus', 'European Rabbit', 'Rabbit', 7, 'herbivore', 'Kitchen Zone', 2, 'M', 'cooking', 'cooks who are slow '),
+
+  new Animal('Meles Meles', 'European Badger', 'Clive Badger, Esq.', 5, 'omnivore', 'Litigation Zone', 1, 'M', 'loyalty', 'reckless behavior'),
+
+  new Animal('Castor Fiber', 'European Beaver', 'Beaver', 4, 'herbivore', 'Litigation Zone', 1, 'M', 'helping Mr. Fox', 'loud noises'),
+
+  new Animal('Mustela Nivalis', 'Common Weasel', 'Wes Anderson', 5, 'carnivore', 'American Film Director Zone', 0, 'M', 'all things twee', 'terrible color palettes'),
+
+];
 
 
 }
