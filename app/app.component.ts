e2d2399d8c5a5585c9ef3fbcbfcb5c0d1ea226e0
cj <thatcat13@gmail.com>
2018-01-19 10:08:17 -0800

    add EditAnimalComponent with communication set up between edit and root

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,17 +9,19 @@ import { Animal } from './animal.model';
     <h1>Fantastic Zoo</h1>
     <h3>{{currentAnimalList}}</h3>
     <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
+    <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()"></edit-animal>
   </div>
   `
 })
 
 
 export class AppComponent {
-  currentAnimalList: string = 'Animals currently residing here: ';
+  currentAnimalList: string = 'Animals currently residing here:';
   //component class is what this is declares how component BEHAVES
+  selectedAnimal = null;
 
   masterAnimalList: Animal[] = [
-  new Animal('Lutra Lutra', 'European Otter', 'Linda Otter', 3, 'pescetarian', 'River Zone', 2, 'F', 'fish, typing', 'loud noises'),
+  new Animal('Lutra Lutra', 'European Otter', 'Linda Otter', 3, 'pescetarian', 'Lawyer Office Zone', 2, 'F', 'fish, typing fast', 'loud noises'),
 
   new Animal('Talpa Europa', 'European Mole', 'Mole', 2, 'carnivore', 'Underground Zone', 2, 'M', 'earthworms', 'bright light'),
 
@@ -32,6 +34,14 @@ export class AppComponent {
   new Animal('Mustela Nivalis', 'Common Weasel', 'Wes Anderson', 5, 'carnivore', 'American Film Director Zone', 0, 'M', 'all things twee', 'terrible color palettes'),
 
 ];
+
+editAnimal(clickedAnimal) {
+  this.selectedAnimal = clickedAnimal;
+}
+
+finishedEditing() {
+  this.selectedAnimal = null;
+}
 
 
 }
--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 
 
 @Component({
   selector: 'edit-animal',
   template: `
-
+  <div>
+    <div *ngIf='childSelectedAnimal'>
+      <h3>{{childSelectedAnimal.name}}</h3>
+      <hr>
+      <h3>Edit Animal</h3>
+      <label>Enter Animal Name:</label>
+      <input [(ngModel)]="childSelectedAnimal.name"><br>
+      <label>Enter Age:</label>
+      <input [(ngModel)]="childSelectedAnimal.age"><br>
+      <label>Enter Number of Caretakers Required:</label>
+      <input [(ngModel)]="childSelectedAnimal.caretakers">
+      <button (click)="doneButtonClicked()">Done</button>
+     </div>
+   </div>
   `
 })
 
 export class EditAnimalComponent {
+  @Input() childSelectedAnimal: Animal;
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked() {
+    this.doneButtonClickedSender.emit();
+  }
 
 }
